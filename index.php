4eb04584842c6f336e9d4af019841bafe00a9c7b
EnderDE77 <erliskendezi@gmail.com>
2023-10-21 23:23:36 +0200

    aa

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
+<<<<<<< Updated upstream
 <?php header("Location: ./main/home_page/indexH.php");
+=======
+<?php 
+header("Location: ./main/floor_page/indexF.php");
+>>>>>>> Stashed changes
--- main/floor_page/indexF.php
+++ main/floor_page/indexF.php
@@ -7,13 +7,59 @@ require_once "./floor.controller.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Floor</title>
+    <style>
+        /* Navbar styles */
+        .navbar {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 10px;
+        }
+
+        /* Container for the button row */
+        .button-row {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 20px;
+        }
+
+        /* Style for the buttons */
+        .button {
+            display: inline-block;
+            padding: 15px 30px; /* Increase padding for larger buttons */
+            margin: 5px;
+            background-color: #3498db;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            text-align: center;
+            text-decoration: none;
+            transition: background-color 0.3s;
+        }
+
+        /* Center the text in the buttons */
+        .button span {
+            display: inline-block;
+            vertical-align: middle;
+            line-height: normal;
+        }
+
+        /* Hover effect for the buttons */
+        .button:hover {
+            background-color: #2980b9;
+        }
+    </style>
 </head>
 <body>
+    <div class="button-row">
     <?php foreach($floorList as $floor): ?>
         <form method="GET" action="test.php">
             <input readonly hidden name="building" value=<?= $building?>>
-            <input type="submit" name="floor" value=<?= $floor[0]?>>
+            <button class="button" type="submit" name="floor" value=<?= $floor[0]?>><span><?= $floor[0]?></span></button>
         </form>
     <?php endforeach; ?>
+    </div>
 </body>
 </html>
